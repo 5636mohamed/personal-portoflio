6033e16f951ebc9ac9e6c0dd0e043a505bde0c47
5636mohamed <85107448+5636mohamed@users.noreply.github.com>
2022-09-03 03:46:31 +0200

    some editions

---
+++ js/plugins-other-projects.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-typed", {
+    strings: ["Data Science", "Artificial Intelligence", "Robotics", "IoT", "Quantum Computing"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+})
