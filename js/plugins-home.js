dc557d0589b05e4021b5a1ac4f92a733db8771bf
5636mohamed <85107448+5636mohamed@users.noreply.github.com>
2022-09-27 20:38:31 +0200

    adding main structure and basics in the website

--- js/plugins-home.js
+++ js/plugins-home.js
@@ -19,16 +19,52 @@ window.onload = function (){
 // get all elements in the home page
 
 let about = document.getElementById("about"),
-    education = document.getElementById("education"),
     resume = document.getElementById("resume"),
+    projects = document.getElementById("projects"),
+    connection = document.getElementById("contact"),
     footer = document.querySelector("footer");
 window.onscroll = function (){
-    if (window.scrollY >= (about.offsetTop- 200))
+    if (window.scrollY >= (about.offsetTop - 200))
         about.classList.add("slide-top");
-    if (window.scrollY >= (education.offsetTop- 200))
-        education.classList.add("slide-top");
-    if (window.scrollY >= (resume.offsetTop- 200))
+    if (window.scrollY >= (resume.offsetTop - 200))
         resume.classList.add("slide-top");
+    if (window.scrollY >= (projects.offsetTop - 200))
+        projects.classList.add("slide-top");
+    if (window.scrollY >= (connection.offsetTop - 200))
+        connection.classList.add("slide-top");
     if (window.scrollY >= (footer.offsetTop - 500))
         footer.classList.add("slide-top");
 };
+
+
+/* paste this line in verbatim */
+window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+/* customize formbutton below*/     
+formbutton("create", {
+  action: "https://formspree.io/f/xvoyzznd",
+  title: "How can we help?",
+  fields: [
+    { 
+      type: "email", 
+      label: "Email:", 
+      name: "email",
+      required: true,
+      placeholder: "your@email.com"
+    },
+    {
+      type: "textarea",
+      label: "Message:",
+      name: "message",
+      placeholder: "What's on your mind?",
+    },
+    { type: "submit" }      
+  ],
+  styles: {
+    title: {
+      backgroundColor: "gray"
+    },
+    button: {
+      backgroundColor: "gray"
+    }
+  }
+});
